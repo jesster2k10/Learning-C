ad9cfc3018b64b86b767a365dcb0dad40f52e60e
Jesse Onolememen <jesster2k10@gmail.com>
2017-04-24 10:24:22 +0100

    I learnt variables

--- Learning C/main.cpp
+++ Learning C/main.cpp
@@ -12,6 +12,51 @@
 using namespace std;
 
 int main() {
-    cout << "My name is Jesse Onolemememen\n";
+    /*
+    int x = 0;
+    int cars = 14;
+    int debt = -1000;
+    float cash = 2.32;
+    double credit = 32.32;
+    char a = 'a';
+    char* sandwich = "sandwich";
+    bool does_she_like_me = true;
+    auto do_i_have_pants_on = false;
+    auto blank_check = 200.00;
+    int over_9000 = INT_MAX;
+    
+    cout << over_9000 << endl;
+    
+    int _ = 5;
+    
+    cout << _ << endl;
+    
+    cout << sizeof(char) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(bool) << endl;
+    
+    cout << ' ' << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(long long int) << endl; */
+    
+    /*
+    signed y = -1;
+    unsigned int x = -1;
+    
+    cout << x << endl;
+    cout << y << endl; */
+    
+    cout << INT_MIN << endl;
+    cout << INT_MAX << endl;
+    cout << LONG_MIN << endl;
+    cout << LONG_MAX << endl;
+    cout << SHRT_MIN << endl;
+    cout << SHRT_MAX << endl;
+    cout << USHRT_MAX << endl;
+
+    
     return 0;
 }
