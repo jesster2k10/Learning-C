318dda7b0e33f21c5f15381ada5f4a4170cedadd
Jesse Onolememen <jesster2k10@gmail.com>
2017-04-24 08:40:22 +0100

    Initial Commit

---
+++ Learning C/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learning C
+//
+//  Created by Jesse Onolememen on 24/04/2017.
+//  Copyright © 2017 Jesse Onolememen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
